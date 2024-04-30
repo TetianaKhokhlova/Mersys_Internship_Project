eee75427437a33342d7b4e4929abf0bdf61a347a
Hanna <aniasagan99@gmail.com>
2024-05-01 00:26:30 +0300

    Hanna 04

---
+++ src/test/java/features/_04_HamburgerMenuMessaging.feature
@@ -0,0 +1,22 @@
+Feature:
+
+  Background:
+    Given I navigate to the Mersys Login page
+    And   I enter correct username and password
+      |Student_10|
+      |S12345|
+    Then  I press Login button
+
+    Scenario:
+      Given I need to click on the hamburger menu
+      And I should be able to click on the Messaging
+      And I should be able to click on the New message
+      Then I need to click on the hamburger menu2
+      And I should be able to click on the Messaging2
+      And I should be able to click on the inbox button
+      Then I need to click on the hamburger menu3
+      And I should be able to click on the Messaging3
+      And I should be able to click on the outbox button
+      Then I need to click on the hamburger menu4
+      And I should be able to click on the Messaging4
+      Then I should be able to click on the trash button
---
+++ src/test/java/pages/_04_MessagingPage.java
@@ -0,0 +1,38 @@
+package pages;
+
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.FindBy;
+import utilities.CommonMethods;
+
+
+public class _04_MessagingPage extends CommonMethods {
+
+    @FindBy(xpath= "(//fa-icon[@class='ng-fa-icon mat-mdc-tooltip-trigger'])[6]")
+    public WebElement hamburgerMenu;
+
+
+    @FindBy(xpath = "(//*[name()='polygon'])[1]")
+    public WebElement messagingLink;
+
+    @FindBy(xpath = "//span[contains(text(),'New Message')]")
+    public WebElement newMessageButton;
+
+    @FindBy(xpath = "//span[contains(text(),'Inbox')]")
+    public WebElement inboxButton;
+
+    @FindBy(xpath = "//span[contains(text(),'Outbox')]")
+    public WebElement outboxButton;
+
+    @FindBy(xpath = "//span[4]//span[1]//button[1]//span[1]//fa-icon[1]//*[name()='svg']")
+    public WebElement trashButton;
+
+
+
+}
+
+
+
